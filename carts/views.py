1d7237604d6cbe2eb19dc9b333a670cceebd7a79
kalash <kalashm24@gmail.com>
2020-05-30 17:11:28 +0545

    Billing profile in Checkout View

--- carts/views.py
+++ carts/views.py
@@ -6,6 +6,10 @@ from .models import Cart
 
 from orders.models import Order
 
+from billing.models import BillingProfile
+
+from accounts.forms import LoginForm, GuestForm
+
 # Create your views here.
 # def cart_create(user=None):
 #     cart_obj = Cart.objects.create(user=None)
@@ -41,4 +45,18 @@ def checkout_home(request):
         return redirect('cart:home')
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request, 'carts/checkout.html' , {'object':order_obj})
+
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+
+    if user.is_authenticated:
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
+        user=user)
+
+    context = {
+        'object': order_obj,
+        'billing_profile': billing_profile,
+        'login_form': login_form
+    }
+    return render(request, 'carts/checkout.html' , context)
